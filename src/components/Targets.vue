6dc4b888e10b15db5bca1c26d813bc03e4d3dd05
madorTill <mador.till@gmail.com>
2024-07-23 17:35:19 +0300

    start target

--- src/components/Targets.vue
+++ src/components/Targets.vue
@@ -1,67 +1,72 @@
 <template>
-    <div id="targets">
-        <!-- the title and info -->
-    <div class="div-text">
-      <h2 class="title-text">מטרות</h2>
-      <p class="info-text">
-        לתדריך קיימות מספר מטרות:
-      </p>
-      <p class="info-text comment-text">
-        - לחצ/י על המסך -
-    </p>
-    </div>
+    <div id="targets" @click="showNextTarget">
+        <!-- Title and information -->
+        <div class="div-text">
+            <h2 class="title-text">מטרות</h2>
+            <p class="info-text">
+                לתדריך קיימות מספר מטרות:
+            </p>
+            <p class="info-text comment-text">
+                - לחצ/י על המסך -
+            </p>
+        </div>
 
-    <div v-for="target in targetsArray" :key="target" class="target-container">
-        <p class="text">{{ targetsArray[target] }}</p>
-        
-    </div>
+        <!-- Loop through targetsArray to display targets -->
+        <div v-for="(target, index) in targetsArray" :key="index" class="target-container" :class="targetNum >= index ?  'target' + index : ''" >
+            <p class="text">{{ target }}</p>
+        </div>
 
+        <!-- the arrows -->
+        <!-- <img  v-for="(i, index) in 4" :key="index" src="../../src/assets/media/arrowTarget.svg" alt="arrow" class="arrow" :class="'arrow' + index" :style="targetNum >= index ? 'animation: shoot1 2s ease-in-out forwards' : '' "/> -->
 
-    <img src="../../src/assets/media/target.png" alt="target" class="target"/>
+        <img  v-for="(i, index) in 4" :key="index" src="../../src/assets/media/arrowTarget.svg" alt="arrow" class="arrow" :class="['arrow' + index , targetNum >= index ? `shoot${index}`: '' ]"/>
+
+        <!-- Static image -->
+        <img src="../../src/assets/media/target.png" alt="target" class="target"/>
     </div>
 </template>
 
 <script>
-
 export default {
-    name: 'targets',
-    props: [],
-    components: {
-        
-    },
+    name: 'Targets',
     data() {
         return {
-            targetsArray: ['הקטנת אי ודאות אצל הנחנך', 'יצירת הבנה ותמונה שלמה', 'הגדלת סיכויי הצלחת החניך', 'כלי לשיפור העברת הלמידה'],
+            targetsArray: [
+                'הקטנת אי ודאות אצל הנחנך',
+                'יצירת הבנה ותמונה שלמה',
+                'הגדלת סיכויי הצלחת החניך',
+                'כלי לשיפור העברת הלמידה'
+            ],
+            targetNum: -1,
         };
     },
     methods: {
-      
-    },
- 
+        showNextTarget() {
+            this.targetNum++;
+        }
+    }
 }
 </script>
 
 <style scoped>
+#targets {
+    width: 100vw;
+    height: 100vh;
+}
 .div-text {
-
-  text-align: center;
+    text-align: center;
 }
 
 .title-text {
-  margin: auto;
-  font-size: 3rem;
-  padding-top: 2%;
+    margin: auto;
+    font-size: 3rem;
+    padding-top: 2%;
 }
 
 .info-text {
-  position: relative;
-  /* padding: 0% 20% 0 35%; */
-  /* right: 8%; */
-  font-size: 1.9rem;
-  display: block;
-  z-index: 1;
-  color: #3a3737;
-  margin-bottom: 0%;
+    font-size: 1.9rem;
+    color: #3a3737;
+    margin-bottom: 0%;
 }
 
 .comment-text {
@@ -84,13 +89,103 @@ export default {
     background-color: white;
     text-align: center;
     color: black;
+    position: absolute;
+    box-shadow: 0 15px 20px -20px rgba(0, 0, 0, 0.4);
+    animation: fade 2s ease-in-out;
+    display: none;
 }
 
 .text {
     color: black;
     position: absolute;
-    top:50%;
+    top: 50%;
     left: 50%;
     transform: translate(-50%, -50%);
+    margin: 0;
+    font-size: 1.5rem;
+}
+
+.target0 {
+    left: 13%;
+    display: block;
+
+}
+
+.target2 {
+    right: 13%;
+    display: block;
+}
+
+.target1 {
+    left: 13%;
+    bottom: 10%;
+    display: block;
+
+}
+
+.target3 {
+    right: 13%;
+    bottom: 10%;
+    display: block;
+}
+
+@keyframes fade {
+  from {
+    opacity: 0; /* Start with opacity 0 (fully transparent) */
+  }
+  to {
+    opacity: 1; /* End with opacity 1 (fully opaque) */
+  }
+}
+
+.arrow {
+    width: 15rem;
+    position: absolute;
+    bottom: -10%;
+    z-index: 3;
+    /* left: 40%;
+    transform: rotate(10deg); */
+}
+
+.arrow0 {
+    left: 40%;
+    transform: rotate(10deg);
+}
+.arrow1 {
+    left: 45%;
+    transform: rotate(10deg);
+}
+.arrow2 {
+    left: 50%;
+    transform: rotate(10deg);
+}
+.arrow3 {
+    left: 55%;
+    /* transform: sc(-10deg); */
+}
+.shoot0 {
+    animation: shoot0 2s ease-in-out forwards;
+}
+
+.shoot1 {
+    animation: shoot0 2s ease-in-out forwards;
+}
+@keyframes shoot0 {
+    from {
+        bottom: -10%;
+    }
+    to {
+        bottom: 10%;
+    }
+    
+} 
+@keyframes shoot1 {
+    from {
+        bottom: -10%;
+    }
+    to {
+        bottom: 10%;
+    }
+    
 }
 </style>
